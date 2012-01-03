ce3b8760f2035a78044170d93e3730af2cd223aa
James Halliday <mail@substack.net>
2012-01-03 12:36:16 -0800

    modified tests for [ 0, x - 1 ]

--- test/coord.js
+++ test/coord.js
@@ -4,9 +4,9 @@ var coord = require('../');
 test('pixel to gl', function (t) {
     var tr = coord([ 400, 300 ], [ [ -1, 1 ], [ -1, 1 ] ]);
     t.deepEqual(tr(0, 0), [ -1, -1 ], 'bottom left');
-    t.deepEqual(tr(200, 150), [ 0, 0 ], 'origin');
-    t.deepEqual(tr([ 200, 150 ]), [ 0, 0 ], 'origin array');
-    t.deepEqual(tr(400, 0), [ 1, -1 ], 'bottom right');
+    t.deepEqual(tr(199, 149), [ 0, 0 ], 'origin');
+    t.deepEqual(tr([ 199, 149 ]), [ 0, 0 ], 'origin array');
+    t.deepEqual(tr(399, 0), [ 1, -1 ], 'bottom right');
     t.end();
 });
 
@@ -30,8 +30,8 @@ test('gl to pixel', function (t) {
 test('flip gl to pixel', function (t) {
     var tr = coord([ [ 1, -1 ], [ 1, -1 ] ], [ 400, 300 ]);
     t.deepEqual(tr(1, 1), [ 0, 0 ], 'bottom left');
-    t.deepEqual(tr(0, 0), [ 200, 150 ], 'origin');
-    t.deepEqual(tr(-1, 1), [ 400, 0 ], 'bottom right');
+    t.deepEqual(tr(0, 0), [ 199, 149 ], 'origin');
+    t.deepEqual(tr(-1, 1), [ 399, 0 ], 'bottom right');
     t.end();
 });
 
